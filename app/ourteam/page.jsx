6367d79d1305bf17448d8437c756ab7b5ab33ae1
Muzammil Ali <muzammilali28@gmail.com>
2023-05-08 00:44:12 +0500

    OurTeam Page, now each route via next/link has folder structure with a page.jsx init

---
+++ app/ourteam/page.jsx
@@ -0,0 +1,23 @@
+export const metadata = {
+    title: "Our Team"
+}
+
+const OurTeamPage = () => {
+    return (
+        <div>
+            <h1>Our Team</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur 
+                adipisicing elit. Repudiandae 
+                vitae ducimus doloribus. Dolore 
+                maxime ea officiis cumque ullam 
+                maiores quod, veritatis, 
+                dolorem reiciendis quia id. 
+                Odio voluptate modi 
+                repudiandae a?
+            </p>
+        </div>
+    )
+}
+
+export default OurTeamPage;
