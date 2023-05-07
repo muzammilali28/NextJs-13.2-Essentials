67764c1c552ca1de38859b3543ebac03dea560de
Muzammil Ali <muzammilali28@gmail.com>
2023-05-08 00:45:08 +0500

    About Page, now each route via next/link has folder structure with a page.jsx init

---
+++ app/about/page.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const metadata = {
+    title: "About Page"
+}
+
+const AboutPage = () => {
+
+    return (
+        <div>
+            <h1>About Traversy Media</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur 
+                adipisicing elit. Repudiandae 
+                vitae ducimus doloribus. Dolore 
+                maxime ea officiis cumque ullam 
+                maiores quod, veritatis, 
+                dolorem reiciendis quia id. 
+                Odio voluptate modi 
+                repudiandae a?
+            </p>
+        </div>
+    )
+}
+
+export default AboutPage
