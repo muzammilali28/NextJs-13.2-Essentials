e11753fc837d4885e0f073283b3fb9ad8e8f022a
Muzammil Ali <muzammilali28@gmail.com>
2023-05-25 16:18:37 +0500

    Loading Spinner Animation overwritten specifically for Todos

---
+++ app/components/CustomLoader.jsx
@@ -0,0 +1,9 @@
+const CustomLoader = () => {
+    return (
+        <div className='todo-loader'>
+            <div className="todo-spinner"></div>
+        </div>
+    )
+}
+
+export default CustomLoader
