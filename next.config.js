5aba597296ba77c3e65795c0d8c20a79d57b497d
Muzammil Ali <muzammilali28@gmail.com>
2023-05-25 16:12:51 +0500

    Added Mongoose as serverCompExtPackage to make mongoose work in Next Js plus added serveraction to use it in the app

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    
+    // If In case you are using 'mongoose' to call to the API make sure to add this below line, else you will get alot of errors
+    serverComponentsExternalPackages: ["mongoose"],
+
+    // Latest Update Next Js 13.4 , Server Action, make sure to add this below line to you the server action feature.
+    serverActions: true,
   },
 }
 
